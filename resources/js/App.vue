765f0ad6bd32f73b425528443f96d072f28c3e64
Ayat <ayat2486@gmail.com>
2019-05-25 21:25:53 +0600

    item create system app(full CRUD)

---
+++ resources/js/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div>
+            
+                <router-view></router-view>
+            
+        </div>
+    </div>
+</template>
+
+<style>
+    .fade-enter-active, .fade-leave-active {
+      transition: opacity .5s
+    }
+    .fade-enter, .fade-leave-active {
+      opacity: 0
+    }
+</style>
+
+<script>
+import Example from './components/ExampleComponent.vue';
+
+    export default{
+        name: 'app',
+        components: {
+            Example,
+            
+        }
+    }
+</script>
---
+++ resources/js/components/CreateItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <h1>Create An Item</h1>
+    <form v-on:submit.prevent="addItem">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Item Name:</label>
+            <input type="text" class="form-control" v-model="item.names">
+          </div>
+        </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Item Price:</label>
+              <input type="text" class="form-control col-md-6" v-model="item.prices" />
+            </div>
+          </div>
+        </div><br />
+        <div class="form-group">
+          <button class="btn btn-primary">Add Item</button>
+        </div>
+    </form>
+  </div>
+</template>
+<script>
+  export default {
+    data(){
+        return{
+          item:{}
+        }
+    },
+    methods: {
+      addItem(){
+        let uri = 'http://localhost:8000/items';
+        this.axios.post(uri, this.item).then((response) => {
+          this.item.names = '';
+          this.item.prices = '';
+        })
+        .catch(error => {
+          console.log('error');
+        })
+        
+    }
+  }
+}
+</script>
